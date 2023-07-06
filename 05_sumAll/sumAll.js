70adf8a08bebc9b1eee222a491bafacc45a903fe
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-06 16:30:15 -0700

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (
+        num1 < 0 ||
+        num2 < 0 ||
+        !Number.isInteger(num1) ||
+        !Number.isInteger(num2)
+        )
+    {
+        return 'ERROR';
+    }
+    const diff = Math.abs(Math.max(num1, num2) - Math.min(num1, num2));
+    console.log(diff);
+    let sum = 0;
+    for (let i = 0; i <= diff; i++)
+    {
+        sum += Math.min(num1, num2) + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

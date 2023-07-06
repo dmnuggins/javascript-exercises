555a4fd3bd2fd517913f0e90beced433bfa22dcc
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-06 15:58:48 -0700

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,29 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    reversed = '';
+    const inputArray = inputString.split('');
+    left = 0;
+    right = inputString.length - 1
+    temp = ''
 
+    if(!inputString)
+    {
+        return reversed;
+    }
+    else
+    {
+        while(left < right)
+        {
+            temp = inputArray[left];
+            inputArray[left] = inputArray[right];
+            inputArray[right] = temp;
+
+            left += 1;
+            right -= 1;
+        }
+        reversed = inputArray.join('');
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line

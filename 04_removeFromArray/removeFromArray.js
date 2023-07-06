b8b150208613ec100de85d0845611cec1fb55bd0
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-06 16:18:17 -0700

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elements) {
 
+
+    for (let e of elements) {
+        const index = inputArray.indexOf(e);
+        if (index !== -1) {
+            inputArray.splice(index, 1);
+        }
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line

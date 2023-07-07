7ca232e0f16714835df5dde497f2d7eb9eda8a08
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-06 17:29:29 -0700

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = parseFloat(((tempF - 32) * 5) / 9).toFixed(1);
+
+  // tempC = Math.round((tempC * 10) / 10);
+
+  console.log(tempC);
+
+  return parseFloat(tempC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = parseFloat((tempC * (9/5)) + 32).toFixed(1);
+
+  // tempF = Math.round((tempF * 10) / 10);
+
+  console.log(tempF);
+
+  return parseFloat(tempF);
 };
 
 // Do not edit below this line

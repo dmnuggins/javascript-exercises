5385c2d39a62616414713ddb2069b3e6c58b26b1
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-06 17:09:58 -0700

    06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
 
+    if (inputYear % 4 == 0)
+    {
+        if (inputYear % 100 == 0)
+        {
+            if (inputYear % 400 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
